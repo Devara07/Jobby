3747328cd164a10374cbb5936dd273083ce9647e
Dheeraj <dheeraj2032006@gmail.com>
2025-09-01 10:22:08 +0530

    fixed a width issue, still working on jobs/abc

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,11 @@
 import Home from './components/Home'
-import { BrowserRouter, Route, Routes } from 'react-router';
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import LoginForm from './components/LoginForm';
 import Jobs from './components/Jobs';
 import ProtectedRoute from './components/ProtectedRoute';
 import JobDetails from './components/JobDetails'
+import NotFoundjobs from './components/NotFoundjobs/index.jsx';
+import JobsLayout from './components/JobsLayout/index.jsx';
 
 const App = () => (
   <BrowserRouter>
@@ -12,12 +14,14 @@ const App = () => (
         <ProtectedRoute><Home /></ProtectedRoute>
       } />
       <Route path="/login" element={<LoginForm />} />
-      <Route path="/jobs" element={
-        <ProtectedRoute><Jobs /></ProtectedRoute>
-      } />
-      <Route path="/jobs/:id" element={
-        <ProtectedRoute><JobDetails /></ProtectedRoute>
-      } />
+
+      <Route path="/jobs" element={<JobsLayout />}>
+        <Route index element={<ProtectedRoute><Jobs /></ProtectedRoute>} />
+        <Route path=":id" element={<ProtectedRoute><JobDetails /></ProtectedRoute>} />
+        <Route path="*" element={<NotFoundjobs />} />
+      </Route>
+
+      <Route path="*" element={<NotFoundjobs />} />
     </Routes>
   </BrowserRouter>
 )
--- src/components/NotFoundjobs/index.jsx
+++ src/components/NotFoundjobs/index.jsx
@@ -1,7 +1,8 @@
+import './index.css'
 
 const NotFoundJobs = () => {
     return(
-        <div>
+        <div className='not-found-container'>
         <img src='https://assets.ccbp.in/frontend/react-js/failure-img.png' alt='not found' className='not-found-image' />
         </div>
     )
